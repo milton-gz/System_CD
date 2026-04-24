0d8e8cc2afe507dd387ae705dfb3f274c554f2fb
Marcos <quintanillamarcos468@gmail.com>
2026-04-24 13:35:20 -0600

    subir comandos.bash para mostrar los comandos de git

--- comandos.bash
+++ comandos.bash
@@ -1,4 +1,9 @@
 git status              # ver cambios del proyecto
 git add .               # preparar todos los archivos
 git commit -m "agregar el mensaje gg"  # guardar cambios con mensaje
-git push origin main    # subir todo a GitHub
+git push origin quintanilla    # subir todo a GitHub
+
+git status
+git add .
+git commit -m "subir comandos.bash para mostrar los comandos de git"
+git push origin quintanilla
