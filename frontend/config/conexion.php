07a2c2cd42aee2a17be68e64dc60632b73edc031
Marcos <quintanillamarcos468@gmail.com>
2026-04-27 15:19:27 -0600

    subir bd clinica

--- frontend/config/conexion.php
+++ frontend/config/conexion.php
@@ -1,15 +1,15 @@
 <?php
+// Configuración de la conexión a la base de datos
+$host = "localhost"; // Dirección del servidor de la base de datos
+$username = "root"; // Nombre de usuario para la conexión
+$password = ""; // Contraseña para la conexión
+$database = "clinica"; // Nombre de la base de datos
 
-define("BASE_URL", "/System_CD/");
-
-$host = "sql300.infinityfree.com";
-$username = "if0_40501117";
-$password = "R8m&in7-d(jDi/6";
-$database = "if0_40501117_clinica";
-
+// Crear una nueva instancia de mysqli para conectar a la base de datos
 $conn = new mysqli($host, $username, $password, $database);
 
+// Verificar si la conexión falló
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("Error de conexión: " . $conn->connect_error); // Terminar el script y mostrar el error
 }
-?>
+?>  
