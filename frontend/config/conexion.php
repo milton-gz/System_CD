80b1d2d4f932f0626615c2a44588e0c5950bd937
Marcos <quintanillamarcos468@gmail.com>
2026-04-27 16:27:32 -0600

    subir bd

--- frontend/config/conexion.php
+++ frontend/config/conexion.php
@@ -1,15 +1,19 @@
 <?php
-// Configuración de la conexión a la base de datos
-$host = "localhost"; // Dirección del servidor de la base de datos
-$username = "root"; // Nombre de usuario para la conexión
-$password = ""; // Contraseña para la conexión
-$database = "clinica"; // Nombre de la base de datos
+// Configuración de la conexión a la base de datos en Clever Cloud
+$host = "bdfaie2b9mu0vhhk0rxv-mysql.services.clever-cloud.com"; // Host remoto
+$username = "uyiktzva7pwy8etd"; // Usuario
+$password = "buESWSI3tphIHx6SKMEG"; // Contraseña
+$database = "bdfaie2b9mu0vhhk0rxv"; // Nombre de la BD
+$port = 3306; // Puerto (importante)
 
-// Crear una nueva instancia de mysqli para conectar a la base de datos
-$conn = new mysqli($host, $username, $password, $database);
+// Crear conexión
+$conn = new mysqli($host, $username, $password, $database, $port);
 
-// Verificar si la conexión falló
+// Verificar conexión
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error); // Terminar el script y mostrar el error
+    die("Error de conexión: " . $conn->connect_error);
 }
-?>  
+
+// Opcional: establecer charset (recomendado)
+$conn->set_charset("utf8");
+?>
